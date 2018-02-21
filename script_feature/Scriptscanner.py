1a4852b6cce00b32d96c6c4552821db1a93f069e
nw20hewo <nw20hewo@studserv.uni-leipzig.de>
2018-02-21 20:16:02 +0100

    Add scriptscanner

---
+++ script_feature/Scriptscanner.py
@@ -0,0 +1,54 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import re
+
+def klammernloeschen (str):
+    klammerauf = str.find('(')
+    klammerzu = str.find(')')
+
+    while (klammerauf != -1 and klammerzu != -1):
+
+        if(klammerauf<klammerzu):
+            str = str[:klammerauf] + str[klammerzu+1:]
+
+
+        klammerauf = str.find('(')
+        klammerzu = str.find(')')
+    print(str)
+    return
+
+
+def get_script(str):
+    my_url = str
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html,"html.parser")
+            #find_all_b = page_soup.find_all(re.compile("b"))
+
+    text = page_soup.get_text()
+
+
+
+    #print(text)
+    return text
+
+
+'''klammernloeschen(script)
+
+i = 0
+for t in script:
+    if '/n' in script:
+        i += 1
+'''
+
+
+script = get_script("http://www.imsdb.com/scripts/Deadpool.html")
+text=''
+for lines in script.splitlines(True):
+    if lines.startswith("           "):
+        text=text+lines
+        print(text)
+
+
+print(klammernloeschen(text))        
