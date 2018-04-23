3e82af58a87b3ccda2864fb6e83b57b59d19f127
nw20hewo <nw20hewo@studserv.uni-leipzig.de>
2018-04-23 14:08:46 +0200

    add Pie chart wordsspoken improved: liest dict nach value aus!

--- script_feature/16.April.py
+++ script_feature/16.April.py
@@ -1,5 +1,46 @@
-from textblob import TextBlob
+import re
+from collections import Counter, defaultdict
+#from sys import stdin
+from pprint import pprint
+import matplotlib.pyplot as plt
 
-wiki = TextBlob("Python is a high-level, general-purpose programming language.")
+#Liest Script ein
+f = open("/Users/Nils/MEGAsync/Dokumente/Uni/3. Semester/DH/IntroDH17/La_la_land_script.txt", "r")
+script = f.read()
 
-wiki.tags
+#bestimmt den gesprochenen Anteil
+words_spoken = defaultdict(Counter)
+currently_speaking = 'Narrator'
+
+for line in script.split('\n'):
+    name = line.replace('(CONT\'D)', '').strip()
+    if re.match('^[A-Z]+$', name):
+        currently_speaking = name
+    else:
+        words_spoken[currently_speaking].update(line.split())
+
+
+pprint(words_spoken)
+
+character1 = input("Enter name of character:")
+character2 = input("Enter name of another character:")
+
+sumchar1 = sum(words_spoken[character1].values())
+sumchar2 = sum(words_spoken[character2].values())
+#print(sum(words_spoken[character].values()))
+
+
+#Diagramme
+
+labels = [character1,character2]
+values = [sumchar1,sumchar2]
+
+
+word_counts = [sumchar1, sumchar2]
+names = [character1, character2]
+colors = ['r', 'y']
+plt.pie(word_counts, labels=names, colors=colors, startangle=90, autopct='%.1f%%')
+plt.title('Research on words spoken')
+plt.show()
+
+f.close()
